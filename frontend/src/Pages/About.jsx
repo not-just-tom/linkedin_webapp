07bd34f7c5d733664efe3a07d17973ad28bf0154
not-just-tom <tpcshotton@gmail.com>
2024-09-09 13:45:23 +0100

    mobile-friendlier-check

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -3,25 +3,25 @@ import "./About.css"
 export function About() {
     return(
         <>
-            <header>Welcome to my Website:)</header>
+            <h1>Welcome to my Website:)</h1>
             <h2>I am Tom</h2>
             <br/>
             <content>
                 <div>
-                <h1>Professional Experience</h1>
+                <h2>Professional Experience</h2>
                 <ul>
                     <li>    
-                    <job>Website and Computing Officer</job>
+                    <h4>Website and Computing Officer</h4>
                     <p>3 years using JavaScript, html, css</p>
                     </li>
                     <br/>
                     <li>    
-                    <job>Backend Software Developer</job>
+                    <h4>Backend Software Developer</h4>
                     <p>9 month project using MERN stack, Python </p>
                     </li>
                     <br/>
                     <li>    
-                    <job>Retirement and Investment Actuarial Intern, Aon</job>
+                    <h4>Retirement and Investment Actuarial Intern, Aon</h4>
                     <p>3 months using VBA, Python modelling financial markets</p>
                     </li>
                    
@@ -29,21 +29,21 @@ export function About() {
                 </ul>
                 </div>
                 <div>
-                <h1>Academic Experience</h1>
+                <h2>Academic Experience</h2>
                 <ul>
                     <li>    
-                    <job>Mathematics Bsc, University of Oxford</job>
+                    <h4>Mathematics Bsc, University of Oxford</h4>
                     <p>Overall grade: 2:2</p>
                     </li>
                     <br/>
                     <li>    
-                    <job>Mathematics and Data Science, University of Bath</job>
+                    <h4>Mathematics and Data Science, University of Bath</h4>
                     <p>Grade: Predicted 2:2</p>
                     </li>
                     <br/>
                     <li>    
-                    <job>4 month Portfolio Optimisation Project</job>
-                    <p></p>
+                    <h4>4 month Portfolio Optimisation Project</h4>
+                    <p>Independently assessed group work</p>
                     </li>
                 </ul>
                 </div>
