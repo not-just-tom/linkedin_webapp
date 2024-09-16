405a2681dcce7d3a620a6606c7f1cc8798ad16ac
not-just-tom <tpcshotton@gmail.com>
2024-09-16 14:52:17 +0100

    new

--- backend/chatbot/chat.py
+++ backend/chatbot/chat.py
@@ -4,12 +4,12 @@ import json
 import torch
 
 
-from chatbot.model import NeuralNet
-from chatbot.nltk_utils import bag_of_words, tokenize
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('..\backend\chatbot\intents.json', 'r') as json_data:
+with open('chatbot\intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "backend\chatbot\data.pth"
