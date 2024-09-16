b2720744eec26d284abb822cc27f526776754d4f
not-just-tom <tpcshotton@gmail.com>
2024-09-16 15:34:08 +0100

    new

--- backend/chatbot/chat.py
+++ backend/chatbot/chat.py
@@ -4,8 +4,8 @@ import json
 import torch
 
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from chatbot.model import NeuralNet
+from chatbot.nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
